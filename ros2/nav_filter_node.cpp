b2103ded37a1ae686b545a7b75c15da54e110bca
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2025-06-02 15:08:17 -0300

    feat: create lifecycle node

--- ros2/nav_filter_node.cpp
+++ ros2/nav_filter_node.cpp
@@ -4,24 +4,37 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+
 #include "std_msgs/msg/string.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
 
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
 
 using namespace std::chrono_literals;
+using rclcpp_lifecycle::LifecycleNode;
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
-class NavFilter : public rclcpp::Node
+class NavFilter : public LifecycleNode
 {
   public:
-    NavFilter()
-    : Node("nav_filter"), count_(0)
+    NavFilter(const rclcpp::NodeOptions & options = rclcpp::NodeOptions())
+    : LifecycleNode("nav_filter", options),
+      tf_buffer_(this->get_clock()),
+      tf_listener_(tf_buffer_)
     {
+      this->declare_parameter<std::string>("base_link", "base_link");
+      this->declare_parameter<std::string>("imu_link",  "base_link");
+      this->declare_parameter<std::string>("twist_link",  "base_link");
+      this->declare_parameter<std::string>("pose_link",  "base_link");
+
       publisher_ = this->create_publisher<nav_msgs::msg::Odometry>("/odom", 10);
 
       imu_subscription_ = this->create_subscription<sensor_msgs::msg::Imu>(
@@ -32,6 +45,29 @@ class NavFilter : public rclcpp::Node
 
       twist_stamped_subscription_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(
       "/twist", 10, std::bind(&NavFilter::twist_callback, this, std::placeholders::_1));
+    }
+  protected:
+    CallbackReturn on_configure(const rclcpp_lifecycle::State &)
+    { 
+      RCLCPP_INFO(get_logger(), "Configuring Navigation Filter");
+      try
+      {
+        rclcpp::Time now = this->get_clock()->now();
+
+        imu_transform_ = tf_buffer_.lookupTransform(
+                "base_link", "imu_link", tf2::TimePointZero);
+        twist_transform_ = tf_buffer_.lookupTransform(
+                "imu_link", "twist_link", tf2::TimePointZero);
+        pose_transform_ = tf_buffer_.lookupTransform(
+                "imu_link", "pose_link", tf2::TimePointZero);
+      }
+      catch(const tf2::TransformException & ex)
+      {
+        RCLCPP_WARN(this->get_logger(), "Could not get transform: %s", ex.what());
+        return CallbackReturn::FAILURE;
+      }
+
+      return CallbackReturn::SUCCESS;
     }
 
   private:
@@ -51,6 +87,8 @@ class NavFilter : public rclcpp::Node
       RCLCPP_INFO(this->get_logger(), "Twist message received");
     }
 
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
 
     rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_;
 
@@ -58,13 +96,18 @@ class NavFilter : public rclcpp::Node
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_stamped_subscription_;
     rclcpp::Subscription<geometry_msgs::msg::TwistStamped>::SharedPtr twist_stamped_subscription_;
 
+    geometry_msgs::msg::TransformStamped imu_transform_;
+    geometry_msgs::msg::TransformStamped twist_transform_;
+    geometry_msgs::msg::TransformStamped pose_transform_;
+
     size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<NavFilter>());
+  auto filter_node = std::make_shared<NavFilter>();
+  rclcpp::spin(filter_node->get_node_base_interface());
   rclcpp::shutdown();
   return 0;
 }

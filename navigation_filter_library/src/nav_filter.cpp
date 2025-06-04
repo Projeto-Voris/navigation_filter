769b16a23e886f05cdde6265a7f4ac70fa8aa31c
Gustavo Alcantara <gustavo.v.alcantara@gmail.com>
2025-06-04 16:34:05 -0300

    feat: load sensors in nav filter class

--- navigation_filter_library/src/nav_filter.cpp
+++ navigation_filter_library/src/nav_filter.cpp
@@ -8,8 +8,8 @@ class NavFilter
 {
 private:
     IMUModel imu;
-    //TwistModel twist;
-    //PoseModel pose;
+    TwistModel twist;
+    PoseModel pose;
 
     const Eigen::Vector3f gravity = Eigen::Vector3f(0, 0, 9.81);
 
@@ -20,14 +20,13 @@ private:
     Eigen::Matrix<float, 3, 1> corr_noise;
 
 public:
-    NavFilter(const IMUModel& imu_model);
+    NavFilter(const IMUModel& imu_model, const TwistModel& twist_model, const PoseModel& pose_model);
     ~NavFilter();
 };
 
-NavFilter::NavFilter(const IMUModel& imu_model) :imu(imu_model)
+NavFilter::NavFilter(const IMUModel& imu_model, const TwistModel& twist_model, const PoseModel& pose_model) :imu(imu_model), twist(twist_model), pose(pose_model)
 {
-    //twist = twist_model;
-    //pose = pose_model;
+    
 }
 
 NavFilter::~NavFilter()
